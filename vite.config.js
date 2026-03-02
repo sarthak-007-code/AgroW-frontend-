017cb0280e9b0fe6a70067ed8a16f985e98e1594
sarthak-007-code <waralesarthak000@gmail.com>
2026-03-02 17:11:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        allowedHosts: [
-            'ag-agrow.trycloudflare.com',
-            'tayna-feirie-xuan.ngrok-free.dev',
-            'portfolio-favorites-holder-wrote.trycloudflare.com',
-            'critics-weekly-depends-qualifications.trycloudflare.com/',
-            'agro-w-frontend-git-main-waralesarthak000-3705s-projects.vercel.app'
-        ],
-        proxy: {
-            '/api': {
-                target: 'https://braided-constrained-maximilian.ngrok-free.dev',
-                changeOrigin: true,
-                headers: {
-                    'ngrok-skip-browser-warning': 'true'
-                }
-            },
-            '/uploads': {
-                target: 'https://braided-constrained-maximilian.ngrok-free.dev',
-                changeOrigin: true,
-                headers: {
-                    'ngrok-skip-browser-warning': 'true'
-                }
-            }
-        }
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      'ag-agrow.trycloudflare.com',
+      'tayna-feirie-xuan.ngrok-free.dev',
+      'portfolio-favorites-holder-wrote.trycloudflare.com',
+      'critics-weekly-depends-qualifications.trycloudflare.com'
+    ],
+    proxy: {
+      '/api': {
+        target: 'https://braided-constrained-maximilian.ngrok-free.dev',
+        changeOrigin: true,
+        headers: { 'ngrok-skip-browser-warning': 'true' }
+      },
+      '/uploads': {
+        target: 'https://braided-constrained-maximilian.ngrok-free.dev',
+        changeOrigin: true,
+        headers: { 'ngrok-skip-browser-warning': 'true' }
+      }
     }
+  }
 })
